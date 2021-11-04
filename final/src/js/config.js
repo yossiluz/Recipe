978fa8064a45008994a5de83bc71322713486fbc
betsalelkogo <betsalelkogo@gmail.com>
2021-11-04 13:20:14 +0200

    Initial commit

---
+++ final/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2f2c2a01-3ef8-44b1-9e6e-f2fb834a08a1';
+export const MODAL_CLOSE_SEC = 2.5;
